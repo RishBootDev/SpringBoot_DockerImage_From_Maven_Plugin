a37516541361c6de9504bd16da266248521fb47f
Rishabh <rishabhdubey1472004@gmail.com>
2026-01-06 22:41:15 +0530

    provided the steps to execute

--- src/main/java/com/rishbootdev/springbootdockerimagefrommaven/controller/AppController.java
+++ src/main/java/com/rishbootdev/springbootdockerimagefrommaven/controller/AppController.java
@@ -1,4 +1,17 @@
 package com.rishbootdev.springbootdockerimagefrommaven.controller;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
 public class AppController {
+
+    @GetMapping
+    public ResponseEntity<String> greet() {
+        return ResponseEntity.ok("Hello Ji, Radhe Radhe");
+    }
 }
